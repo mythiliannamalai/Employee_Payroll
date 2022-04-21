7d50b72f03bdc72c61598f4222d94f521b5c6c2c
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-21 19:42:26 +0530

    creat table

--- Payroll.sql
+++ Payroll.sql
@@ -1,4 +1,11 @@
-
 --UC-1 create database
-
 create database Payroll;
+
+--UC-2 create table
+create table Employee_Payroll(
+Emp_Id int identity not null Primary key,
+Emp_Name varchar(50),
+Salary bigint,
+Joining_Date datetime);
+
+select * from Employee_Payroll
