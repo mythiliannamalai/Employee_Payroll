f87b68571b652d17bc71106373c879785faf9053
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-21 19:47:41 +0530

    insert values

--- Payroll.sql
+++ Payroll.sql
@@ -9,3 +9,11 @@ Salary bigint,
 Joining_Date datetime);
 
 select * from Employee_Payroll
+
+--UC-3 Insert values
+insert into Employee_Payroll(Emp_Name,Salary,Joining_Date) values
+('Mythili',75000,'2022-04-18'),
+('Naveen kumar',100000,'2022-04-18'),
+('Lavanya',40000,'2022-04-18'),
+('Santhosh',50000,'2022-04-18'),
+('Kiruba',40000,'2022-04-18');
